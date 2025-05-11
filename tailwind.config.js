8d8a7a0f0129aa01c313502766cdcbc59f2e59e4
jmurrah <jacobhmurrah@gmail.com>
2025-05-11 08:08:00 -0500

    getting project set up

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./docs/**/*.{html, js}'],
+  theme: {
+    extend: {
+      colors: {
+        // 'custom-background': '#0a0a0a',
+        // 'primary-purple': '#a35bff',
+        // 'secondary-gray': '#b5b5b5',
+        // 'accent-purple': '#9000ff'
+      }
+    },
+  },
+  plugins: [],
+}
